f3a52f550b725df36f79fe21a2c49c56cc01226b
william <willhgreen02@gmail.com>
2021-09-02 16:22:32 -0500

    commit tree 4a8f1a5ccbba1d1df20949bffc474fe3b853eee7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+// Write your solution here! 
+ const cats = ["Milo", "Otis", "Garfield"]; 
+   function destructivelyAppendCat(name) {
+ cats.push("Ralph");}
+    function destructivelyPrependCat(name) {
+        cats.unshift("Bob")
+    }
+    function destructivelyRemoveLastCat() {
+        cats.pop()
+    }
+    function destructivelyRemoveFirstCat() {
+        cats.shift()
+    }
+    function appendCat(name) {
+        const newArray = cats.slice()
+        newArray.push(name)
+        return newArray
+    }
+    function prependCat(name) {
+        const newArray = cats.slice()
+        newArray.unshift("Arnold")
+        return newArray 
+    }
+    function removeLastCat() {
+        const newArray = cats.slice()
+        newArray.pop()
+        return newArray
+    }
+    function removeFirstCat(){
+        const newArray = cats. slice()
+        newArray.shift()
+        return newArray
+    }
